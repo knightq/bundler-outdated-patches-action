3229d55d1846b1ac0eefbedb6ab91fab7b75ec09
Salicetti Andrea <andrea.salicetti@gmail.com>
2023-06-30 14:23:13 +0200

    Check preconditions Gemfile.lock

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
+if test -f "Gemfile"
+then
+  echo "✅ Gemfile exists."
+else
+  echo "❌ Gemfile not found!"
+  exit -1
+fi
+
+if test -f "Gemfile.lock"then
+  echo "✅ Gemfile.lock exists."
+else
+  echo "❌ Gemfile.lock not found!"
+  exit -1
+fi
+
 bundle outdated --local --filter-patch
